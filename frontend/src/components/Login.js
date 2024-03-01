8a4cf3e95f21dbd7d214de7313a80f854dbed5aa
Liam Kilkenny <102427486+kilkennylj@users.noreply.github.com>
2024-03-01 18:54:48 -0500

    No errors, not working due to decode

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useJwt } from "react-jwt";
+import { jwtDecode } from "jwt-decode";
 
 function Login()
 {
@@ -24,25 +25,42 @@ function Login()
 			
 			var res = JSON.parse(await response.text());
 			
-			if ( res.id <= 0 )
+			const { accessToken } = res;
+			const decoded = jwtDecode(accessToken,{complete:true});
+			
+			try
 			{
-				setMessage('User/Password combination incorrect');
+				var ud = decoded;
+				var userId = ud.userId; // might be wrong case
+				var firstName = ud.firstName;
+				var lastName = ud.lastName;
+				
+				if (userId <= 0)
+				{
+					setMessage("User/Password combination incorrect");
+				}
+				
+				else
+				{
+					var user = {firstName:firstName,lastName:lastName,id:userId}
+					localStorage.setItem('user_data', JSON.stringify(user));
+					
+					setMessage('');
+					window.location.href = '/landing';
+				}
 			}
 			
-			else
+			catch(e)
 			{
-				var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
-				localStorage.setItem('user_data',JSON.stringify(user));
-				
-				setMessage('');
-				window.location.href = '/landing';
+				console.log(e.toString());
+				return("");
 			}
 		}
 		
 		catch(e)
 		{
-			alert(e.toString());
-			return;
+			console.log(e.toString());
+			return("");
 		}
 	};
 	
@@ -63,5 +81,4 @@ function Login()
 };
 
 export default Login;
-
 
