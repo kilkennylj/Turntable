d9379e26251cc72f161fc06cd52d1f9b3b84b34d
kilkennylj <boston19rules@outlook.com>
2024-03-01 20:13:02 -0500

    Working Login w/ JWT

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { useJwt } from "react-jwt";
+//import { useJwt } from "react-jwt"; // is said to be required but isn't??
+import { jwtDecode } from "jwt-decode";
 
 function Login()
 {
@@ -24,25 +25,45 @@ function Login()
 			
 			var res = JSON.parse(await response.text());
 			
-			if ( res.id <= 0 )
+			const { accessToken } = res;
+			
+			const decoded = jwtDecode(accessToken,{complete:true});
+
+			try
 			{
-				setMessage('User/Password combination incorrect');
+				var ud = decoded;
+				var userId = ud.userId;
+				var firstName = ud.firstName;
+				var lastName = ud.lastName;
+				
+				if (userId <= 0)
+				{
+					setMessage("User/Password combination incorrect");
+				}
+				
+				else
+				{
+					var user = {firstName:firstName,lastName:lastName,id:userId}
+					localStorage.setItem('user_data', JSON.stringify(user));
+					
+					setMessage('');
+					window.location.href = '/landing';
+				}
 			}
 			
-			else
+			// setMessage was added in, not in MERN C but its required
+			catch(e)
 			{
-				var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
-				localStorage.setItem('user_data',JSON.stringify(user));
-				
-				setMessage('');
-				window.location.href = '/landing';
+				setMessage("User/Password combination incorrect");
+				return("");
 			}
 		}
 		
+		// setMessage was added in, not in MERN C but its required
 		catch(e)
 		{
-			alert(e.toString());
-			return;
+			setMessage("User/Password combination incorrect");
+			return("");
 		}
 	};
 	
