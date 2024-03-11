4bca5741a3225d06768d624881e2bec84d7c1f68
jiadamski <ji_adamski@outlook.com>
2024-03-11 16:58:54 -0400

    Temporary redirect component created for demo

---
+++ frontend/src/components/Redirect.js
@@ -0,0 +1,10 @@
+// FOR DEMONSTRATION PURPOSES ONLY - DELETE AFTER USE - REPLACE WITH <LINK> ELEMENT FROM REACT ROUTER
+export function RedirectToLogin()
+{
+    window.location.href = './login';
+}
+
+export function RedirectToRegister()
+{
+    window.location.href = './register';
+}
