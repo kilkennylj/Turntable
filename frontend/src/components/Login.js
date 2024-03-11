0d2dcfa75a3301eba221fc9c8c8d8bfbe99c8305
jiadamski <ji_adamski@outlook.com>
2024-03-11 16:41:51 -0400

    Login/register components fully functional, with the exception of no check for a duplicate user being made

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 //import { useJwt } from "react-jwt"; // is said to be required but isn't??
 import { jwtDecode } from "jwt-decode";
+// FOR DEMONSTRATION PURPOSES ONLY - DELETE AFTER USE - REPLACE WITH <LINK> ELEMENT FROM REACT ROUTER
+import { RedirectToRegister } from './Redirect.js';
 
 function Login()
 {
@@ -47,7 +49,7 @@ function Login()
 					localStorage.setItem('user_data', JSON.stringify(user));
 					
 					setMessage('');
-					window.location.href = '/albums';
+					window.location.href = '/landing';
 				}
 			}
 			
@@ -68,17 +70,40 @@ function Login()
 	};
 	
 	return(
-		<div id="loginDiv">
-			<form onSubmit={doLogin}>
-			<span id="inner-title">Log In</span><br />
-			<input type="text" id="loginName" placeholder="Username"
-				ref={ (c) => loginName = c} /><br />
-			<input type="password" id="loginPassword" placeholder="Password"
-			ref={ (c) => loginPassword = c} /><br />
-			<input type="submit" id="loginButton" class="buttons" value = "Do It"
-				onClick={doLogin} />
+		<div class="w-screen h-screen flex justify-center items-center">
+			<form class="bg-gray-400 border border-black rounded px-12 pt-8 pb-8" onSubmit={doLogin}>
+				{/* Title text*/}
+				<div class="mt-20 mb-10 md:flex justify-center">
+					<label class="w-56 h-20 text-red-500 text-5xl font-normal font-['Hendangan']">Turntable</label>
+				</div>
+				{/* Username */}
+                <div class="mb-2">
+                    <label class="block w-max text-black text-lg font-bold font-['Lucida Sans Typewriter']">Username</label>
+                    <input class="w-72 h-10 rounded-3xl py-2 px-3 bg-white border border-black appearance-none" type="text" id="loginName" ref={ (c) => loginName = c} /><br />
+                </div>
+				{/* Password */}
+                <div class="mb-0">
+                    <label class="block w-max text-black text-lg font-bold font-['Lucida Sans Typewriter']">Password</label>
+                    <input class="w-72 h-10 rounded-3xl py-2 px-3 bg-white border border-black appearance-none" type="password" id="loginPassword" ref={ (c) => loginPassword = c} /><br />
+                </div>
+				{/* Forgot password subtext */}
+                <div class="mb-2">
+                    <label class="text-black text-xs underline cursor-pointer font-['Lucida Sans Typewriter']">Forgot password?</label>
+                </div>
+				{/* Login Button */}
+                <div class="mb-2 md:flex md:justify-center">
+                    <input class="w-28 h-10 bg-white rounded-3xl font-bold underline cursor-pointer font-['Lucida Sans Typewriter']" type="submit" id="loginButton" value = "Login" onClick={doLogin} />
+                </div>
+				{/* Register a new account subtext */}
+                <div class="mb-2 md:flex md:justify-center">
+                    <label class="pr-2">Don't have an account?</label>
+                    <label class="underline cursor-pointer" onClick={RedirectToRegister}>Register</label>
+                </div>
+				{/* Error message */}
+                <div class="md:flex md:justify-center">
+                    <span id="loginResult">{message}</span>
+                </div>
 			</form>
-			<span id="loginResult">{message}</span>
 		</div>
 	);
 };
