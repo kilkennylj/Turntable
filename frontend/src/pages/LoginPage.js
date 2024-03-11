565924a615d109dad017f975b75e396863c7cfa2
jiadamski <ji_adamski@outlook.com>
2024-03-11 16:53:22 -0400

    Login/register pages minimally styled Very basic landing page temporarily added for demo

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,13 +1,10 @@
 import React from 'react';
-
-import PageTitle from '../components/PageTitle';
 import Login from '../components/Login';
 
 const LoginPage = () =>
 {
 	return(
 		<div>
-			<PageTitle />
 			<Login />
 		</div>
 	);
