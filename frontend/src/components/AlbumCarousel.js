712907290aad19391ea210a2365f0dd95cc08639
arkaik2k01 <nobmiguel2001@gmail.com>
2024-03-27 20:03:34 -0400

    Initial Testing

--- frontend/src/components/AlbumCarousel.js
+++ frontend/src/components/AlbumCarousel.js
@@ -1,53 +1,33 @@
-import React from 'react';
-import Slider from 'react-slick';
-// import './ImageCarousel.css';
+import React, { Component } from "react";
+import Slider from "react-slick";
 
-const ImageCarousel = ({ albums }) => {
-  const [showInfo, setShowInfo] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const handleImageClick = (index) => {
-    setSelectedImage(index);
-    setShowInfo(true);
-  };
-
-  const handleInfoClose = () => {
-    setShowInfo(false);
-  };
-
+function AlbumCarousel() {
   const settings = {
     dots: true,
     dotsClass: "slick-dots slick-thumb",
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
   };
-
   return (
-    <div>
+    <div className="slider-container">
       <Slider {...settings}>
-        {albums.map((image, index) => (
-          <div key={index}>
-            <img src={image.src} alt={image.alt} onClick={() => handleImageClick(index)} />
-          </div>
-        ))}
-      </Slider>
-
-      {showInfo && (
-        <div className="info-modal">
-          <div className="info-content">
-            <h2>Album Title - Artist name</h2>
-            <p>Here goes the album year.</p>
-            <p>Here goes the rating of the album.</p>
-            <p>Here goes the tags of the album.</p>
-            <button onClick={handleInfoClose}>Close</button>
-          </div>
+        <div>
+          <img src={"../testImages/David_Bowie_-_Heroes.png"} />
         </div>
-      )}
+        <div>
+          <img src={"../testImages/David_Bowie_-_Heroes.png"} />
+        </div>
+        <div>
+          <img src={"../testImages/David_Bowie_-_Heroes.png"} />
+        </div>
+        <div>
+          <img src={"../testImages/David_Bowie_-_Heroes.png"} />
+        </div>
+      </Slider>
     </div>
   );
-};
+}
 
-export default ImageCarousel;
+export default AlbumCarousel;
