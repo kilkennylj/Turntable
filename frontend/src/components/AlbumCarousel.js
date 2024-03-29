95f2661fdea269fc02bb277135461a69e9a114dc
arkaik2k01 <nobmiguel2001@gmail.com>
2024-03-28 21:01:03 -0400

    Basic Carousel
    Basic carousel done with small example

--- frontend/src/components/AlbumCarousel.js
+++ frontend/src/components/AlbumCarousel.js
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import "../styles/AlbumCarousel.css";
 
 
 function AlbumCarousel() {
@@ -9,31 +10,32 @@ function AlbumCarousel() {
     customPaging: function(i) {
       return (
         <a>
-          <img src={"/assets/img/David_Bowie_-_Heroes.png"} />
+          <img className="carousel_thumbnail" src={`/assets/img/test_${1+i}.jpg`} />
         </a>
       );
     },
     dots: true,
     infinite: true,
     dotsClass: "slick-dots slick-thumb",
+    centerMode: true,
+    centerPadding: "0px",
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 3,
   };
   return (
     <div className="slider-container">
       <Slider {...settings}>
         <div>
-          <img src={"/assets/img/David_Bowie_-_Heroes.png"} alt="Album cover"/>
+          <img className="carousel_img" src={"/assets/img/test_1.jpg"} alt="Album cover"/>
         </div>
         <div>
-        <img src={"/assets/img/David_Bowie_-_Heroes.png"} alt="Album cover"/>
+        <img className="carousel_img" src={"/assets/img/test_2.jpg"} alt="Album cover"/>
         </div>
         <div>
-        <img src={"/assets/img/David_Bowie_-_Heroes.png"} alt="Album cover"/>
+        <img className="carousel_img" src={"/assets/img/test_3.jpg"} alt="Album cover"/>
         </div>
         <div>
-        <img src={"/assets/img/David_Bowie_-_Heroes.png"} alt="Album cover"/>
+        <img className="carousel_img" src={"/assets/img/test_4.jpg"} alt="Album cover"/>
         </div>
       </Slider>
     </div>
