02b0e9b0d0318195e19ce12132cbf94d6cd5edd7
arkaik2k01 <nobmiguel2001@gmail.com>
2024-04-01 01:55:37 -0400

    Finalized landing page
    Finalized landing page with UI

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,10 +1,39 @@
-import Landing from '../components/Landing';
+import React, { useState, useEffect } from "react";
+import AlbumCarousel from '../components/landing/AlbumCarousel';
+import Topbar from '../components/landing/Topbar';
+import SearchBar from "../components/landing/SearchBar";
+import RippleBackground from "../components/landing/RippleBackground";
+import Album from '../models/Album';
+import "../styles/LandingPage.css"
 
-const LandingPage = () =>
-{
-    return(
-        <div>
-            <Landing />
+const LandingPage = () => {
+
+    useEffect(() => {
+        document.body.style.background = "#1E1E1E";
+      }, []);
+
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = (query) => {
+        console.log("Search query:", query);
+    };
+
+    const handleDelete = (thisCard) => {
+        console.log("Deleting...");
+    }
+
+    return (
+        <div className="main">
+        <div className="top">
+             <Topbar />
+        </div>
+        <div className="background">
+            <RippleBackground />
+        </div>
+        <div className="content">
+            <SearchBar onSearch={handleSearch} />
+            <AlbumCarousel onDelete={handleDelete}/>
+        </div>
         </div>
     );
 }
